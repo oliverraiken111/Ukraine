0c193c76cf5f9ab6bd90e3023e8f8f738a2f9f1f
oliverraiken111 <ethanrosshunter@gmail.com>
2025-05-05 16:03:15 -0400

    Update generate_wsj_syria.py

--- generate_wsj_syria.py
+++ generate_wsj_syria.py
@@ -6,7 +6,19 @@ import json
 
 # WSJ Syria section
 url = "https://www.wsj.com/topics/place/syria"
-headers = {"User-Agent": "Mozilla/5.0"}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+}
 
 response = requests.get(url, headers=headers)
 response.raise_for_status()
